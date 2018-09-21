63f24302387b803a1a8e4a721192edea73025ab6
Runone <ngo.danny11@gmail.com>
2018-09-21 15:08:56 -0700

    Complete LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,28 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    let end = Node(value);
+    if (list.tail != null) {
+        list.tail.next = end;
+      }
+    if (list.head === null) {
+        list.head = end;
+    }
+    list.tail = end;
   };
 
   list.removeHead = function() {
+    let start = list.head;
+    delete list.head;
+    list.head = list.tail;
+    return start.value;
   };
 
   list.contains = function(target) {
+  if ((target === list.head.value) || (target === list.tail.value)) {
+  return true} 
+    else {
+  return false}
   };
 
   return list;
